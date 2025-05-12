cbb26481cf5c32e8b8725de2c4cf5b734ba850f7
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-12 15:58:34 +0300

    adding methods signatures

--- Pojects/SeriesAnalyzer/SeriesAnalyzer/Program.cs
+++ Pojects/SeriesAnalyzer/SeriesAnalyzer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,46 @@ namespace SeriesAnalyzer
 {
     internal class Program
     {
+        static void InputSeries()
+        {
+
+        }
+        static bool Validate(string[] series)
+        {
+            return true;
+        }
+        static void Display(string[] series)
+        {
+
+        }
+        static string[] Reverse(string[] series)
+        {
+            return series;
+        }
+        static string[] Sort(string[] series)
+        {
+            return series;
+        }
+        static string Max(string[] series)
+        {
+            return series[0];
+        }
+        static string Min(string[] series)
+        {
+            return series[0];
+        }
+        static string Average(string[] series)
+        {
+            return series[0];
+        }
+        static int Count(string[] series)
+        {
+            return 0;
+        }
+        static string Sum(string[] series)
+        {
+            return series[0];
+        }
         static void Main(string[] args)
         {
         }
