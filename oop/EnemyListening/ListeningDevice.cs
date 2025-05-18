1967d7a7f4e9359a430c05632ece2e06d7aaa0a5
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-18 11:32:16 +0300

    adding project of enemy-listening

---
+++ oop/EnemyListening/ListeningDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class ListeningDevice
+{
+    public string manufactor;
+    public int batteryValue;
+    public bool active;
+    public string[] speeches;
+    public int speechIndex;
+
+    public ListeningDevice(string manufactor, int batteryValue = 100, bool active = true)
+    {
+        this.manufactor = manufactor;
+        this.batteryValue = batteryValue;
+        this.active = active;
+        this.speechIndex = 0;
+    }
+
+    public void Listen(string speech)
+    {
+        if (!this.active)
+        {
+            Console.WriteLine("Can't listen, Device is Inactive.");
+            return;
+        }
+        Console.WriteLine($"There is a new speech! Speech: {speech}");
+        this.batteryValue -= 5;
+        this.speeches[speechIndex] = speech;
+        this.speechIndex++;
+    }
+}
--- oop/EnemyListening/Program.cs
+++ oop/EnemyListening/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EnemeyListening
+namespace EnemyListening
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Enemy sinwar = new Enemy("Sinwar", 1, "Hamas");
+            sinwar.Message();
         }
     }
 }
