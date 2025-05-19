1d12972916252e7b8445ab5b6103535761e88b04
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-19 12:40:48 +0300

    adding tool-box project

---
+++ Projects/ToolBox/Tool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox
+{
+    internal class Tool
+    {
+        protected string Name;
+        protected int Weight;
+        public Tool(string name, int wheight)
+        {
+            this.Name = name;
+            this.Weight = wheight;
+        }
+        public void Describe()
+        {
+            Console.WriteLine("This is a Tool!");
+        }
+        public void Use()
+        {
+            Console.WriteLine("Tool is being used.");
+        }
+    }
+}
