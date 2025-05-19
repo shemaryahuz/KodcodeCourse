7b4d3debcb7e45a8c8d6f5be4cf9768da78407ce
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-19 12:20:53 +0300

    advanced exercise

---
+++ oop/AdvancedExercises/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example for Exercise 20:
+            School school = new School();
+            Student student1 = new Student("Noa");
+            school.AddStudent(student1);
+            Student student2 = new Student("Lior");
+            school.AddStudent(student2);
+            Student student3 = new Student("Eitan");
+            school.AddStudent(student3);
+            Console.WriteLine("Students:");
+            foreach (Student student in school.students)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+    }
+}
---
+++ oop/AdvancedExercises/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExercises
+{
+    internal class Student
+    {
+        public string Name;
+        public Student(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
