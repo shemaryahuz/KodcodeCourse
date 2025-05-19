41a9accf8d5d167d50a31a927a841c1e10dbb875
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-19 11:14:55 +0300

    added arrmy-exercises

---
+++ oop/FootSoldier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class FootSoldier: Soldier
+    {
+        private string _Rifle;
+        public FootSoldier(string name, string rank, int persNum, string rifle): base(name, rank, persNum)
+        {
+            this._Name = name;
+            this._Rank = rank;
+            this._PersNum = persNum;
+            this._Rifle = rifle;
+            this.Organization += ": FootForce";
+        }
+    }
+}
---
+++ oop/Soldier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Soldier
+    {
+        protected string _Name;
+        protected string _Rank;
+        protected int _PersNum;
+        public string Organization = "IDF";
+        public Soldier(string name, string rank, int persNum)
+        {
+            this._Name = name;
+            this._Rank = rank;
+            this._PersNum = persNum;
+        }
+        public void Greet()
+        {
+            Console.WriteLine($"Hello {this._Rank} {this._Name}. Your Personal Number is: {this._PersNum}");
+        }
+        public void Servise()
+        {
+            Console.WriteLine($"You are serving in the {this.Organization}");
+        }
+    }
+}
