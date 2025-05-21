5022cd0596ed36972250cea5981c340ca82e797e
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-21 07:41:25 +0300

    added and imlemented GroundTrap class

---
+++ Projects/BattleArena/GroundTrap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena
+{
+    internal class GroundTrap: IPowerable
+    {
+        public bool IsOn { get; set; }
+        public void TurnOn()
+        {
+            this.IsOn = true;
+        }
+        public void TurnOff()
+        {
+            this.IsOn = false;
+        }
+    }
+}
