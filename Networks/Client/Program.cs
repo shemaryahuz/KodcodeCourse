087932e0897b58f63aa59e10aab0ad020084fe3e
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-26 13:02:36 +0300

    initial project for client

---
+++ Networks/Client/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.Json;
+
+namespace Client
+{
+    internal class CatData
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+
+    }
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://api.thecatapi.com/v1/images/search";
+            var response = await client.GetAsync(url);
+            //
+            Console.WriteLine($"Response: {response}");
+            string answer = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"String Answar: {answer}");
+            var json = JsonSerializer.Deserialize<CatData>(answer);
+
+        }
+    }
+}
