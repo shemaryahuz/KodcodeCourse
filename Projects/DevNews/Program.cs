6c1986aa3486f381924cf88b7eb908cdcae846b3
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-26 16:52:42 +0300

    added class for exercise 1 in project dev-news

--- Projects/DevNews/Program.cs
+++ Projects/DevNews/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace DevNews
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Task 1 Fetch and display the 5 recent titles
+            HttpResponseMessage postsResponse =  await FetchRecentPosts.FetchAllPosts();
+            string postsStr = await FetchRecentPosts.ConvertToString(postsResponse);
+            NewsPost[] postsArr = FetchRecentPosts.ConvertToNewsPosts(postsStr);
+            FetchRecentPosts.ShowLastPosts(postsArr);
+        }
+    }
+}
--- Projects/DevNews/obj/Debug/net8.0/DevNews.AssemblyInfo.cs
+++ Projects/DevNews/obj/Debug/net8.0/DevNews.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevNews")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0860478732edb5da1e7b008b858a928b7aa9b619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e97b620f29d46916e9c3e9230ff04df6a0719d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevNews")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevNews")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
