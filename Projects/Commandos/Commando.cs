ae851b99d3f617691ad9c46002d4968fde3f8145
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-27 10:17:50 +0300

    added metthods to commando class

--- Projects/Commandos/Commando.cs
+++ Projects/Commandos/Commando.cs
@@ -12,5 +12,26 @@ namespace Commandos
         public string CodeName;
         public string[] Tools;
         public string Status;
+        public Commando(string name, string codeName)
+        {
+            this.Name = name;
+            this.CodeName = codeName;
+            this.Tools = new string[] { "Hammar", "Chisel", "Rope", "bag", "Water" };
+            this.Status = "Standing";
+        }
+        public void Walk()
+        {
+            Console.WriteLine($"Commando {this.Name} is Walking.");
+            this.Status = "Walking";
+        }
+        public void Hide()
+        {
+            Console.WriteLine($"Commando {this.Name} is Hiding.");
+            this.Status = "Hiding";
+        }
+        public void Attack()
+        {
+            Console.WriteLine($"Commando Code Name {this.CodeName} is Attacking.");
+        }
     }
 }
