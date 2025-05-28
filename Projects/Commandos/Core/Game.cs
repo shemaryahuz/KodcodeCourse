9590ae8373b31c215d0f9a46a074ec89105773b6
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-28 16:49:57 +0300

    added game

--- Projects/Commandos/Core/Game.cs
+++ Projects/Commandos/Core/Game.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Commandos.Core
+namespace Commandos
 {
-    internal class Game
+    internal static class Game
     {
+        public static void CreateEnemies()
+        {
+            // Add enemies
+            EnemyFactory enemyFactory = EnemyFactory.GetInstance();
+            enemyFactory.AddEnemy("Ali");
+            enemyFactory.AddEnemy("Achmad");
+            enemyFactory.AddEnemy("Jhon");
+            enemyFactory.AddEnemy("Bob");
+        }
+        public static void CreateCommandos()
+        {
+            // Add commandos
+            CommandoFactory commandoFactory = CommandoFactory.GetInstance();
+            commandoFactory.AddCommando("Avi", "12");
+            commandoFactory.AddCommando("Abe", "34", "Commando");
+            commandoFactory.AddCommando("Albert", "56", "AirCommando");
+            commandoFactory.AddCommando("David", "78", "SeaCommando");
+        }
     }
 }
--- Projects/Commandos/obj/Debug/net8.0/Commandos.AssemblyInfo.cs
+++ Projects/Commandos/obj/Debug/net8.0/Commandos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc427d27706628830033572f3f350dede754e012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e996e856d1a2f2b18a7d358fc15bedc25e2c0a66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commandos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
