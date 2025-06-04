82cf51a6bd414efb53148d58ad2afbe547360ef5
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-06-04 11:29:29 +0300

    added select exercise

---
+++ SQL/select-exe.sql
@@ -0,0 +1,27 @@
+USE classicmodels;
+-- SELECT
+
+SELECT *
+FROM orders;
+
+SELECT lastName, firstName
+FROM employees;
+
+SELECT officeCode
+FROM offices;
+
+-- SELECT DISTINCT 
+SELECT DISTINCT customerName
+FROM customers;
+
+SELECT DISTINCT firstName
+FROM employees;
+
+-- WHERE
+SELECT *
+FROM products
+WHERE quantityInStock < 200;
+
+SELECT *
+FROM offices
+WHERE country='USA';
