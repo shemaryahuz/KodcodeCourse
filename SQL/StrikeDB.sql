8ef6dd2c1dd0b22037d6ccc67ec07b2ddc0b3f99
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-06-05 09:33:07 +0300

    updated strike_db

--- SQL/StrikeDB.sql
+++ SQL/StrikeDB.sql
@@ -21,3 +21,24 @@ ID int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Name varchar(255),
 PowerLevel int CHECK(PowerLevel BETWEEN 1 AND 3)
 );
+
+-- Create Intel Table
+CREATE TABLE intel(
+    ID int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TerroristName varchar(255),
+    Location varchar(255),
+    GatheredAt datetime,
+    ConfidenceScore int CHECK(ConfidenceScore BETWEEN 1 AND 100)
+    );
+	
+-- Create Strikes Table
+CREATE TABLE strikes(
+    ID int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TargetName varchar(255),
+    WeaponUsed varchar(255),
+    OfficerName varchar(255),
+    IntelSource varchar(255),
+    AmmoUsed varchar(255),
+    Result varchar(255),
+    StrikeTime datetime
+    )
