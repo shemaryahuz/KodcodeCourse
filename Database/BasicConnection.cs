ce1a43c2d8b999820e42134581f319c58688ab46
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-06-08 10:46:09 +0300

    initialized project and added basic connection

---
+++ Database/BasicConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Database
+{
+    internal class BasicConnection
+    {
+        private string connStr = "server=localhost;username=root;password=;database=classicmodels";
+        private MySqlConnection connection;
+        public void GetData(string query)
+        {
+            try
+            {
+                connection = new MySqlConnection(connStr);
+                connection.Open();
+                Console.WriteLine("Connection to MySql Database 'cllasicmodels' succseeded");
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string status = reader.GetString("status");
+                    Console.WriteLine($"Status: {status}");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                connection.Close();
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Database/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicConnection connection = new BasicConnection();
+            string query = "SELECT * FROM orders";
+            connection.GetData(query);
+        }
+    }
+}
